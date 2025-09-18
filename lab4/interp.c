6e2c4bf90d8c4c2f9ec26ca1bb5996dd8d8e3e07
gpalmaj <gabrieljavaroni2@gmail.com>
2025-09-18 13:02:45 -0300

    lab 4 feito

--- lab4/interp.c
+++ lab4/interp.c
@@ -1,5 +1,61 @@
+#include <math.h>
 
+#define PI 3.141592653589793
 
 void regular (int n, double a, double b, double* xi){
     
+    // a = x[0]
+    //b = x[n-1]
+
+    for(int i=0; i < n; i++){
+        xi[i] = a + (i * (b-a)/(n-1));
+    }
+
 }
+
+void chebyshev (int n, double a, double b, double* xi){
+
+    int beta = 1;
+    for(int i=0; i<n; i++){
+        xi[i] = (b-a)/2 * cos(beta*PI/(2*n))+(a+b)/2;
+        beta +=2;
+    }
+
+}
+
+double cddn(int n, double* xi, double (*f)(double), int i, int j) {
+    if (i == j) {
+        return f(xi[i]);
+    }
+
+
+    return (cddn(n, xi, f, i+1, j) - cddn(n, xi, f, i, j-1)) / (xi[j] -xi[i]);
+}
+
+void coeficientes (int n, double* xi, double (*f) (double), double* bi){
+    for (int i = 0; i < n; i++) {
+          bi[i] = cddn(n, xi, f, 0, i);  // bi = F[x0...xi]
+      }
+}
+
+double avalia (int n, double* xi, double* bi, double x){
+    double resultado = bi[0];
+    
+    for (int i=1;i<n;i++){
+        double produto = bi[i];
+        for (int j=0; j<i; j++){
+            produto *= (x-xi[j]);
+        }
+        resultado +=produto;
+    
+    }
+
+ 
+
+
+    return resultado;
+}
+
+
+
+
--- lab4/main.c
+++ lab4/main.c
@@ -19,7 +19,7 @@ int main (void)
   
   printf("Amostragem regular [-2, 2]\n");
   for (int i = 2; i <= N; i++) {
-    // TODO: calcular i amostras regulares
+    regular(i, -2.0, 2.0, xi);
     printf("%d: ", i);
     for (int j = 0; j < i; j++)
       printf("%.6f ", xi[j]);
@@ -27,22 +27,22 @@ int main (void)
   } 
 
   printf("\nCoeficientes usando amostras regulares\n");
-  // TODO: calcular 10 amostras regulares [-2, 2]
-  // TODO: calcular 'coeficientes' com as amostras regulares
+  regular (N, -2.0, 2.0, xi);
+  coeficientes(N, xi, f, bi);
   for (int i=0; i < N; ++i)
     printf("%g ", bi[i]);
   printf("\n");
 
   printf("\nAvalia com coeficientes de amostras regulares\n");
   for (float x=-2.0; x < 2.0001; x+=0.1) {
-    double y = -1; // TODO: chamar 'avalia' passando valores de xi, bi e x
+    double y = avalia(N, xi, bi, x);
     double yf = f(x);
     printf("x: %.1f y: %f f(x): %f (y-f(x)): %g\n",x,y,yf,fabs(y-yf));
   }
 
   printf("\nAmostragem chebyshev [-2, 2]\n");
   for (int i = 2; i <= N; i++) {
-    // TODO: calcular amostras chebyshev
+    chebyshev(i, -2.0, 2.0, xi);
     printf("%d: ", i);
     for (int j = 0; j < i; j++)
       printf("%.6f ", xi[j]);
@@ -50,15 +50,15 @@ int main (void)
   } 
   
   printf("\nCoeficientes usando amostras chebyshev\n");
-  // TODO: calcular 10 amostras chebyshev [-2, 2]
-  // TODO: calcular 'coeficientes' com as amostras chebyshev
+  chebyshev(N, -2.0, 2.0, xi);
+  coeficientes(N, xi, f, bi);
   for (int i=0; i < N; ++i)
     printf("%g ", bi[i]);
   printf("\n");
 
   printf("\nAvalia com coeficientes de amostras chebyshev\n");
   for (float x=-2.0; x < 2.0001; x+=0.1) {
-    double y = -1; // TODO: chamar 'avalia' passando valores de xi, bi e x
+    double y = avalia(N, xi, bi, x);
     double yf = f(x);
     printf("x: %.1f y: %f f(x): %f (y-f(x)): %g\n",x,y,yf,fabs(y-yf));
   }
